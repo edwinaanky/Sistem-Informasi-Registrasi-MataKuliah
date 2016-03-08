3703befc64dee3483e9ea41d0f9ac193e95cc353
grahamdesmon <graham.desmon.simanjuntak@gmail.com>
2016-03-08 13:22:42 +0700

    first commit

--- Tubes_SisfoRegistMK/src/tubes_sisforegistmk/Matakuliah.java
+++ Tubes_SisfoRegistMK/src/tubes_sisforegistmk/Matakuliah.java
@@ -9,13 +9,6 @@ package tubes_sisforegistmk;
  *
  * @author desmoncode
  */
-public class Tubes_SisfoRegistMK {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Matakuliah {
     
 }
