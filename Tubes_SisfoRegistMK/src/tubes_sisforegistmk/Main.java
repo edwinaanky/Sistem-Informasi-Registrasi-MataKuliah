8fd346c079f9819776648a46b5e7084bd53e6a13
grahamdesmon <graham.desmon.simanjuntak@gmail.com>
2016-03-26 11:47:43 +0700

    Penambahan Mainmenu

--- Tubes_SisfoRegistMK/src/tubes_sisforegistmk/Main.java
+++ Tubes_SisfoRegistMK/src/tubes_sisforegistmk/Main.java
@@ -5,30 +5,17 @@
  */
 package tubes_sisforegistmk;
 
+import java.io.IOException;
+
 /**
  *
  * @author desmoncode
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Aplikasi apps = new Aplikasi();
         apps.mainMenu();
 
-//        File f = new File("admin.txt");
-////
-//        try (FileInputStream fis = new FileInputStream(f);
-//                ObjectInputStream ois = new ObjectInputStream(fis);) {
-//            
-////        }
-//            Admin a = (Admin) ois.readObject();
-//            while (a != null) {
-//                daftarAdmin.add(a);
-//                a = (Admin) ois.readObject();
-//            }
-//            
-//
-//        } catch (Exception e) {
-//        }
     }
 }
