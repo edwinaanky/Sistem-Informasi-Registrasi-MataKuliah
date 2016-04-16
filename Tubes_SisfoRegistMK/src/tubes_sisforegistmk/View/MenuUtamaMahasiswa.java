97896451f48dd48763de4575834946c3b740be67
Asus <Asus@User>
2016-04-16 19:33:04 +0700

    gui menu utama mahasiswaa

--- Tubes_SisfoRegistMK/src/tubes_sisforegistmk/View/MenuUtamaMahasiswa.java
+++ Tubes_SisfoRegistMK/src/tubes_sisforegistmk/View/MenuUtamaMahasiswa.java
@@ -7,7 +7,7 @@ package tubes_sisforegistmk.View;
 
 /**
  *
- * @author Asus
+ * @author edwina
  */
 public class MenuUtamaMahasiswa extends javax.swing.JFrame {
 
@@ -254,9 +254,12 @@ public class MenuUtamaMahasiswa extends javax.swing.JFrame {
                         .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jButton2)
-                            .addComponent(jButton3))
+                            .addComponent(jButton3)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jButton2)
+                                    .addComponent(jButton1))))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())
